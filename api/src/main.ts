66550f4a7f9388162dadf220607083973fad1b52
Ivo Pereira <ivoecpereira@gmail.com>
2021-12-29 21:08:57 +0000

    Add tag Stores and ValidationPipe as global pipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -7,11 +8,14 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
     .setTitle('DoctorPrice')
     .setDescription('DoctorPrice API')
     .setVersion('1.0')
     .addTag('Products')
+    .addTag('Stores')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
